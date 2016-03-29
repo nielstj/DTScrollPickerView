5690b840966482a3a45f647ab885f70850fc536e
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2016-03-29 16:09:23 +0800

    - Carthage integration -

---
+++ Example/DTScrollPickerView/DTScrollPickerView.h
@@ -0,0 +1,19 @@
+//
+//  DTScrollPickerView.h
+//  DTScrollPickerView
+//
+//  Created by Daniel Tjuatja on 29/3/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTScrollPickerView.
+FOUNDATION_EXPORT double DTScrollPickerViewVersionNumber;
+
+//! Project version string for DTScrollPickerView.
+FOUNDATION_EXPORT const unsigned char DTScrollPickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTScrollPickerView/PublicHeader.h>
+
+
